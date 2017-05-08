991238a42679b1baaf62475af09ff8e1efbc8c6b
Vinay Mehta <anand.kumar@berylsystems.com>
2017-05-08 10:41:21 +0530

    practice for rest api and serilizers

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,44 @@
+class Api::V1::UsersController < ApplicationController
+	#skip_before_filter :verify_authenticity_token 
+	skip_before_action :verify_authenticity_token
+	respond_to :json
+
+	def index
+		respond_with User.all
+	end
+
+	def show
+		respond_with User.find(params[:id])
+	end
+
+	def create
+		user = User.new(user_params)
+		if user.save
+			render json: user, status: 201
+		else
+			render json: { errors: user.errors }, status: 422
+		end
+	end
+
+	def update
+		user = User.find(params[:id])
+		if user.update(user_params)
+			render json: user, status: 200
+		else
+			render json: { errors: user.erros}, status: 422
+		end
+	end
+
+	def destroy
+		user = User.find(params[:id])
+		user.destroy
+		head 204 
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:email,:password, :password_confirmation)
+	end
+end
+
